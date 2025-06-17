41a46b0dfe6329387bbb18085343bd1f57fbfc18
stcucufa <githubnyou@romulusetrem.us>
2025-06-17 23:23:15 +0200

    Line drawing (rough)

--- telepathy.js
+++ telepathy.js
@@ -1,4 +1,36 @@
 import Scheduler from "./epistrophy/lib/scheduler.js";
+import { First } from "./epistrophy/lib/fiber.js";
 
 Scheduler.run().
-    effect(() => { console.info("TELEPATHY"); });
+    repeat(fiber => fiber.
+        event(document, "pointerdown", {
+            eventWasHandled(event, fiber) {
+                fiber.value = event;
+                event.preventDefault();
+            }
+        }).
+        exec(({ value: event }) => {
+            const line = document.querySelector("svg").appendChild(
+                document.createElementNS("http://www.w3.org/2000/svg", "line")
+            );
+            line.setAttribute("x1", event.clientX);
+            line.setAttribute("x2", event.clientX);
+            line.setAttribute("y1", event.clientY);
+            line.setAttribute("y2", event.clientY);
+            line.setAttribute("stroke", "#1d2b53");
+            return line;
+        }).
+        spawn(fiber => fiber.
+            repeat(fiber => fiber.
+                event(document, "pointermove", {
+                    eventWasHandled(event, { value: line }) {
+                        line.setAttribute("x2", event.clientX);
+                        line.setAttribute("y2", event.clientY);
+                    }
+                })
+            )
+        ).
+        spawn(fiber => fiber.event(document, "pointerup")).
+        spawn(fiber => fiber.event(document, "pointercancel")).
+        join(First())
+    );
